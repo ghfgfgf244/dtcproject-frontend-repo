09716a6f4a9f0badb509cdcb6abb6a6ad6b7a011
NTT24 <137974751+NTT24@users.noreply.github.com>
2026-03-10 08:53:08 +0700

    fixing build errors

--- app/(manager)/admin/enrollment-managers/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserTable from "@/components/admin/UserTable";
-
-export default function EnrollmentManagersPage() {
-  return <UserTable role="Enrollment Manager" />;
-}
--- app/(portal)/students/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserTable from "@/components/admin/UserTable";
-
-export default function StudentsPage() {
-  return <UserTable role="Student" />;
-}
