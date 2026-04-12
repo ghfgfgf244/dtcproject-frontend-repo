62486bd313171f5fadaf2fb2c9a26b38dce80e0d
Hung <134862035+ghfgfgf244@users.noreply.github.com>
2026-04-12 11:17:28 +0700

    Remove initialUsers prop from UserClientView

--- app/(manager)/training-manager/users/page.tsx
+++ app/(manager)/training-manager/users/page.tsx
@@ -25,7 +25,7 @@ export default function UserManagementPage() {
           <Breadcrumbs items={breadcrumbsItems} />
         </div>
 
-        <UserClientView initialUsers={combinedUsers} />
+        <UserClientView  />
       </div>
     </div>
   );
